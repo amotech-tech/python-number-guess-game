23123cc2ae7e969bbc1ba58391c85f27a1c9752d
Amos <amongash489@gmail.com>
2026-02-23 22:49:33 +0300

    Added attempt limit and Error handling

--- number-guess.py
+++ number-guess.py
@@ -1,16 +1,26 @@
 import random
 secret_number=random.randint(0,10)
+max_attempts=3
 attempts=0
 print('Guessing a number between 1 and 10')
-while True: 
-    guess=int(input("Enter your guess"))
-    attempts+=1
-    if guess==secret_number:
-        print("You quessed right.")
-        print("Attempts:",attempts)
-        break
-    elif guess<secret_number:
-        print("Too low,Try again.")
-    else:
-        print("Too high, Try again")
+print("you have 3 attempts.\n")
+while attempts<max_attempts: 
+    try:
+        guess=int(input("Enter your guess"))
+        attempts+=1
+        if guess==secret_number:
+            print("You quessed right.")
+            print("Attempts:",attempts)
+            break
+        elif guess<secret_number:
+            print("Too low")
+        else:
+            print("Too high")
+
+        print("attempts left:",max_attempts-attempts,"\n")
+    except ValueError:
+      print("Please enter a valid number.\n")
+if attempts==max_attempts and guess !=secret_number:
+   print("game over")
+   print("the correct number was:",secret_number)
 
